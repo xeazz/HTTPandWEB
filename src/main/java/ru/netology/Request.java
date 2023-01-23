f553f6e387f6c905012c8cb450de36692b9b5757
Dmitri Vorobev <vorobiev1401@mail.ru>
2023-01-23 17:15:13 +0300

    Query Params

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,14 +1,33 @@
 package ru.netology;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 public class Request {
 
+    public static final String GET = "GET";
+    public static final String POST = "POST";
     private final String method;
     private final String path;
+    private final List<String> headers;
+    private final List<NameValuePair> queryParams;
 
 
-    public Request(String requestMethod, String requestPath) {
-        this.method = requestMethod;
-        this.path = requestPath;
+    public Request(String method, String path, List<String> headers,
+                   List<NameValuePair> queryParams) {
+        this.method = method;
+        this.path = path;
+        this.headers = headers;
+        this.queryParams = queryParams;
     }
 
     public String getMethod() {
@@ -19,4 +38,102 @@ public class Request {
         return path;
     }
 
+    public List<String> getHeaders() {
+        return headers;
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+
+    public static Request createRequest(BufferedInputStream in) throws IOException, URISyntaxException {
+        final var allowedMethods = List.of(GET, POST);
+        // лимит на request line + заголовки
+        final var limit = 4096;
+
+        in.mark(limit);
+        final var buffer = new byte[limit];
+        final var read = in.read(buffer);
+
+        // ищем request line
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+//            badRequest(out);
+            return null;
+        }
+
+        // читаем request line
+        final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+        if (requestLine.length != 3) {
+//            badRequest(out);
+            return null;
+        }
+
+        final var method = requestLine[0];
+        if (!allowedMethods.contains(method)) {
+//            badRequest(out);
+            return null;
+        }
+
+        final var path = requestLine[1];
+        if (!path.startsWith("/")) {
+//            badRequest(out);
+            return null;
+        }
+
+        // ищем заголовки
+        final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final var headersStart = requestLineEnd + requestLineDelimiter.length;
+        final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+//            badRequest(out);
+            return null;
+        }
+
+        // отматываем на начало буфера
+        in.reset();
+        // пропускаем requestLine
+        in.skip(headersStart);
+
+        final var headersBytes = in.readNBytes(headersEnd - headersStart);
+        final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+        if (!method.equals(GET)) {
+            in.skip(headersDelimiter.length);
+            // вычитываем Content-Length, чтобы прочитать body
+            final var contentLength = extractHeader(headers);
+            if (contentLength.isPresent()) {
+                final var length = Integer.parseInt(contentLength.get());
+                final var bodyBytes = in.readNBytes(length);
+                final var body = new String(bodyBytes);
+            }
+        }
+        List<NameValuePair> params = URLEncodedUtils.parse(new URI(path), StandardCharsets.UTF_8);
+        return new Request(method, path, headers, params);
+    }
+
+
+    private static Optional<String> extractHeader(List<String> headers) {
+        return headers.stream()
+                .filter(o -> o.startsWith("Content-Length"))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
+
+    // from google guava with modifications
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
+    }
 }
+
+
